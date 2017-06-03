f17eb15baa403713ddaf8a611cbd0375e2338c27
Brothers In Web <brothersinweb@gmail.com>
2017-06-03 13:17:22 +0200

    Uradjena je samo galerija 'najveci zid'

--- najvecizid.php
+++ najvecizid.php
@@ -1,102 +1,95 @@
 
 
 <?php include 'header.html'; ?>
+<link rel="stylesheet" href="css/galerija.css">
+<link rel="stylesheet" href="css/lightbox.css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="js/script.js">
-
-    $(document).ready(function() {
-        $().click(function() {
-            $().show();
-
-        });
-   
-    });
-</script>
-
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/lightbox.min.js"></script>
 <body>
     <h2 style="text-align:center;margin:0 auto;color:#333; padding:14px;">Najveci zid Evrope</h2>
     <div class='galery-box'>
        
             <div class="wrapper">
                  <ul>               
-               <div class="img-single"><li>
-                   <img src="slike/zile/najveci-zid/zid2.jpg" alt="potporni zid"></li>
+               <div class="img-single">
+                   <a href="slike/zile/najveci-zid/zid2.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid2m.jpg" alt="potporni zid"></a>
                 </div>
                 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid3.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid3.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid3m.jpg" alt="potporni zid"></a>
                 </div>
 
                <div class="img-single">
-                   <li> <img src="slike/zile/najveci-zid/zid4.jpg" alt="potporni zid"> </li>
+                   <a href="slike/zile/najveci-zid/zid4.jpg" data-lightbox="galerija"> <img src="slike/zile/najveci-zid/zid4m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid6.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid6.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid6m.jpg" alt="potporni zid"></a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid7.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid6.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid7m.jpg" alt="potporni zid"></a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid8.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid8.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid8m.jpg" alt="potporni zid"></a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid9.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid8.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid9m.jpg" alt="potporni zid"> </a>
                 </div>
 
               <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid11.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid11.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid11m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid12.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid12.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid12m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid14.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid14.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid14m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid15.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid15.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid15m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid16.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid16.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid16m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid17.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid17.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid17m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid18.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid18.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid18.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid19.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid19.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid19m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid20.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid20.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid20m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid21.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid21.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid21m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid22.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid22.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid22m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                   <li> <img src="slike/zile/najveci-zid/zid23.jpg" alt="potporni zid"> </li>
+                   <a href="slike/zile/najveci-zid/zid23.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid23m.jpg" alt="potporni zid"> </a>
                 </div>
 
                <div class="img-single">
-                    <li><img src="slike/zile/najveci-zid/zid13.jpg" alt="potporni zid"> </li>
+                    <a href="slike/zile/najveci-zid/zid13.jpg" data-lightbox="galerija"><img src="slike/zile/najveci-zid/zid13m.jpg" alt="potporni zid"> </a>
                 </div>
 
                 </ul>      
