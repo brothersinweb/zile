e591a821a8216384e0e942bb0683fc977cb8f25d
Brothers In Web <brothersinweb@gmail.com>
2017-06-03 17:49:07 +0200

    Pogledaj dugme

--- najvecizid.php
+++ najvecizid.php
@@ -9,7 +9,7 @@
 <body>
 
 <div class="dugme">
-	<a href="galerija.php"><img  src="slike/dugme.png" alt="Back button" style="width: 80px; height: 80px; margin-left: 30px; margin-top: 20px; position: absolute;"></a>
+	<a href="galerija.php"><img src="slike/dugme.png" alt="Back button" style="width: 80px; height: 80px; margin-left: 30px; margin-top: 20px; position: absolute;"></a>
 
 </div>
     <h2 style="text-align:center;margin:0 auto;color:#333; padding:14px;">Najveci zid Evrope</h2>
