112a191b77435208fa4680c3aba73754cf8bcd98
brothersinweb <brothersinweb@gmail.com>
2017-06-09 18:46:31 +0100

    nav

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
+     
+    if ($(window).width() > 960) {
      $('nav').mouseenter(function(){
          $('.nava').animate({bottom:"0px"},100);
      });
     
      $('nav').mouseleave(function(){
-         $('.nava').animate({bottom:"50px"});
+         $('.nava').animate({bottom:"50px"},50);
      });
+    }else{
+         $('nav').click(function(){
+             $('.nava').toggle(function(){
+         $('.nava').animate({bottom:"0px"},100);
+     });
+    });
+        /*$('nav').toggle(function(){
+         $('.nava').animate({bottom:"50px"},50);
+        });*/
+    }
 
 });
 
