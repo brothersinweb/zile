361a857a25ab4a5e9ba1ba5ac27714a506d8e2d8
brothersinweb <brothersinweb@gmail.com>
2017-06-09 17:32:00 +0100

    nav

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-        $('nav').mouseenter(function(){
-            $('ul').show('fast');
-        });
-    $('nav').mouseleave(function(){
-            $('ul').hide('fasr');
-        });
-
+     $('nav').mouseenter(function(){
+         $('.nava').animate({bottom:"0px"},100);
+     });
+    
+     $('nav').mouseleave(function(){
+         $('.nava').animate({bottom:"50px"});
+     });
 
 });
 
